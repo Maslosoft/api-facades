5ebc603dfe7580025f3c0de6ef0fb4d30331e8da
Piotr Masełkowski <p.maselkowski@applicationpartner.pl>
2026-02-11 19:03:28 +0100

    Added template for interface

--- src/Interfaces/Hydrator.php
+++ src/Interfaces/Hydrator.php
@@ -2,7 +2,15 @@
 
 namespace Maslosoft\ApiFacades\Interfaces;
 
+/**
+ * @template T of object
+ */
 interface Hydrator
 {
+	/**
+	 * @param T $object
+	 * @param array $data
+	 * @return T
+	 */
 	public function hydrate(object $object, array $data): object;
 }
